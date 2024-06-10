eee48c7bbe3c2c6d4bcbbaf728a1ccc08e83bbe3
Michael Nicolaisen <mnicolaisen@uvic.ca>
2024-06-09 19:44:19 -0700

    Button debouncing now works, need to work on menu now

---
+++ button_interrupt.py
@@ -0,0 +1,33 @@
+from machine import Pin
+from machine import Timer
+
+Count = 0
+
+button = Pin(0, Pin.IN, Pin.PULL_UP)
+Button_Active = False
+
+def button_timer_handler(timer):
+    global button
+    global Button_Active
+    #print("In button timer handler")
+    
+    if button.value() == 1 and not Button_Active:
+       # print("Button not Active")
+        Button_Active = True
+       #print("Button is now active")     
+    
+    
+def button_int_handler(x):
+    global Count
+    print("Count: %d" % Count)
+    Count = Count + 1
+    Timer(mode=Timer.ONE_SHOT, period=100, callback=button_timer_handler)
+
+button.irq(trigger = Pin.IRQ_RISING, handler = button_int_handler)
+
+
+
+    
+    
+
+print("button init")
